2b4f57c1437395518e01f77c54ede2613029405d
nilmoreno <nil.moreno@icloud.com>
2016-03-30 18:29:29 +0200

    v1.3.6 - Solución error inicial al abrir la app

--- src/main/java/com/example/alumne/proximocurso/MainFragment.java
+++ src/main/java/com/example/alumne/proximocurso/MainFragment.java
@@ -10,7 +10,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,15 +21,24 @@ import java.util.Map;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment{
-    List<Map<String,Object>> data;
-    String[] titles = {"dam","daw","informatica","multimedia"};
-    String[] images = {"ic_dam","ic_daw","ic_informatica","ic_multimedia"};
+public class MainFragment extends Fragment implements View.OnClickListener{
+    //List<Map<String,Object>> data;
+
+    ArrayList<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
+    HashMap<String, Object> data2 = new HashMap<String, Object>();
+
+    String[] titles = new String[] {"dam","daw","informatica","multimedia"};
+    String[] images = new String[] {"ic_dam","ic_daw","ic_informatica","ic_multimedia"};
+
+    private OnMasterFragmentListener mCallback;
 
     public MainFragment() {
         // Required empty public constructor
     }
 
+    public interface OnMasterFragmentListener {
+        public void onSendButtonClicked(String message);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,6 +49,13 @@ public class MainFragment extends Fragment{
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
+
+        //for testing
+        for(int i=0;i<4;i++)
+        {
+            data2.put(images[i],titles[i]);
+            data.add(data2);
+        }
 
         super.onActivityCreated(savedInstanceState);
         SimpleAdapter adapter = new SimpleAdapter (this.getActivity(),
@@ -55,4 +74,19 @@ public class MainFragment extends Fragment{
 
     }
 
+    @Override
+    public void onClick(View v) {
+
+       // String message = editText.getText().toString();
+        String message = "";
+        if (!message.isEmpty()){
+            //Comunicate with the activity
+            this.mCallback.onSendButtonClicked(message);
+        }else {
+            //Show error message: The message is empty!
+            Toast toast = Toast.makeText(getActivity(), "El mensaje está vacío", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+    }
 }
